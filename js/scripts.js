3f4462820a45c2e8c3e31fe880575b674f67d5e8
HK Kahng and Josh Casuccio <josh@>
2016-08-17 11:31:20 -0700

    add rules to handle initial consonant requirements

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,30 @@ var isLeap = function(whatYear) {
   }
 };
 //Pigs Knees
+var vowels = ["a","e","i","o","u","A","E","I","O","U"];
+var isCon = function(letter) {
+  if (!vowels.includes(letter)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 var pigKnee = function(pigText) {
-  var vowels = ["a","e","i","o","u","A","E","I","O","U"];
   if (vowels.includes(pigText[0])) {
     return pigText + "ay";
   } else if (pigText[0].toLowerCase() === "y") {
     return pigText.substring(1, pigText.length + 1) + "yay";
   }
+  firstCons = [];
+  pigArray = pigText.split("");
+  for (var i = 0; i < pigText.length; i++ ) {
+    if (isCon(pigArray[i])) {
+      firstCons.push(pigArray[i]);
+    } else {
+      i = pigText.length;
+    }
+  };
+  return pigText.substring(firstCons.length, pigText.length) + firstCons.join("") + "ay";
 };
 
 // Interface Code Ist Heer Mein Herr
